671f1af6d02faa8f0355253465888a771f82c074
Marc Fawzi <marc.fawzi@gmail.com>
2013-08-03 14:48:52 -0700

    INITIAL

--- loadCrowbar.js
+++ loadCrowbar.js
@@ -5,10 +5,16 @@
     myScript.src='http://code.jquery.com/jquery-1.10.2.min.js';
     document.getElementsByTagName('body')[0].appendChild(myScript);
   }
+  if (typeof ____l0g____ == 'undefined') {
+    myScript=document.createElement('SCRIPT');
+    myScript.type='text/javascript';
+    myScript.src='http://code.jquery.com/jquery-1.10.2.min.js';
+    document.getElementsByTagName('body')[0].appendChild(myScript);
+  }
   if(typeof ____cr0wB6r____ == 'undefined'){
-   myScript=document.createElement('SCRIPT');
-   myScript.type='text/javascript';
-   myScript.src='https://rawgithub.com/idibidiart/crowbar/master/crowbar.js';
-   document.getElementsByTagName('body')[0].appendChild(myScript);
+    myScript=document.createElement('SCRIPT');
+    myScript.type='text/javascript';
+    myScript.src='https://rawgithub.com/idibidiart/crowbar/master/crowbar.js';
+    document.getElementsByTagName('body')[0].appendChild(myScript);
   }
 })()
