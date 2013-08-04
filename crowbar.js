487ca49fe8b4c1c2ad1749656e7e6a3ca2d9583e
Marc Fawzi <marc.fawzi@gmail.com>
2013-08-03 22:14:20 -0700

    INITIAL

--- crowbar.js
+++ crowbar.js
@@ -11,9 +11,9 @@
             for (var i = 0; i < cssRuleList.length; i++) {
                 cssText += " " + cssRuleList[i].cssText;
             }
-            return cssText + '\n\n';
+            return cssText;
         }
-        return null;
+        return "";
     }
   
   window.onmouseover = function(e) {
@@ -44,46 +44,26 @@
 
        var log = "<!doctype html>\n<html>\n<meta charset='UTF-8'/>\n<style>\n"
 
-       log += getMatchedRules(currEl) + "\n"
+       var rule = getMatchedRules(currEl)
+
+       log += rule ? rule + "\n" : ""
 
        $(currEl).find('*').each(function(){
 
-           log += getMatchedRules(this) + "\n"
+           rule = getMatchedRules(this)
+
+           rule = rule.replace(/[*][ ][{](.*)[}]/g, "")
+
+           log += rule ? rule + "\n" : ""
 
        });
 
       log += "</style>\n<body>\n"
-            + currEl.outerHTML + "\n"
-
-      log += "</body></html>"
-
-      console.log(null)
+            + currEl.outerHTML + "\n</body></html>\n\n"
 
       console.log(log)
 
    }
-
-   // x for extra details
-   if (key == 88) {
-
-      $(currEl).attr("style", $(currEl).attr("style").replace(/outline:(.*);/, ""))
-
-      var log =  "HTML:\n\n" + currEl.outerHTML + "\n\n";
-
-      console.log("Matched CSS Rules:\n\n")
-
-      console.log($(currEl).getClassPath())
-
-      console.log(getMatchedRules(currEl))
-
-      $(currEl).find('*').each(function(){
-
-          console.log($(this).getClassPath())
-
-          console.log(getMatchedRules(this))
-
-      });
-    }
 
     // p for parent
     if (key == 80) {
@@ -101,30 +81,3 @@
   }
   
 })();
-
-(function($){
-    $.fn.extend({
-        getClassPath: function(){
-
-            var rightArrowParents = [];
-
-            var jqParents = $(this).parents()
-            var arr = Array.prototype.slice.call(jqParents, 0)
-
-            arr.pop();
-            arr.unshift(jqParents.context)
-
-            $(arr).each(function() {
-                var entry = this.tagName.toUpperCase();
-                if (this.className) {
-                    entry += "." + this.className.toLowerCase().replace(/ /g, '.');
-                }
-                rightArrowParents.push(entry);
-            });
-
-            rightArrowParents.reverse();
-
-            return rightArrowParents.join(" \u27a5 ");
-        }
-    });
-})(jQuery);
