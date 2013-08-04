68df855192d3281928fd18bf0d0e24ecaf336e2a
Marc Fawzi <marc.fawzi@gmail.com>
2013-08-03 23:40:57 -0700

    INITIAL

--- crowbar.js
+++ crowbar.js
@@ -60,7 +60,7 @@
            rule = getMatchedRules(this)
 
            rule = rule.replace(/\*\s\{(.*)\}/g, "")
-           rule = rule.replace(new RegExp("^[\\s]+$", "g"), "")
+           rule = rule.replace(new RegExp("^[\\s]+$", "gm"), "\n")
 
            if (rule) {
                log += rule;
