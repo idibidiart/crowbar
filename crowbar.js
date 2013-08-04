970a24f174820d4132b58b9928f2ebc8dc474402
Marc Fawzi <marc.fawzi@gmail.com>
2013-08-04 03:40:13 -0700

    INITIAL

--- crowbar.js
+++ crowbar.js
@@ -47,7 +47,7 @@
        )
                 return;
 
-        alert((["p", "h1", "h2", "h3", "h4", "h5", "h6",
+        console.log((["p", "h1", "h2", "h3", "h4", "h5", "h6",
             "ol", "ul", "pre", "address", "blockquote",
             "dl", "div", "fieldset", "form", "output",
             "blockquote", "table", "tfoot",
