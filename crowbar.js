dcf8c92d60b104c7e86945265d4cdb33a8795d38
Marc Fawzi <marc.fawzi@gmail.com>
2013-08-06 01:43:34 -0700

    INITIAL

--- crowbar.js
+++ crowbar.js
@@ -177,7 +177,7 @@
       log += "\n\n";
 
       $(getFontFaceRules()).each(function() {
-          log += String(this) + "\n"
+          log += this.join("") + "\n"
       })
 
       log += "\n\n</style>\n<body>\n<div class='enclosing_styles'>"
