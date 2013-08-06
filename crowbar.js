e8cfef60932d8615fe7e38ecfa93665d90f82701
Marc Fawzi <marc.fawzi@gmail.com>
2013-08-06 09:23:16 -0700

    INITIAL

--- crowbar.js
+++ crowbar.js
@@ -153,7 +153,7 @@
       on = true;
 
       document.body.style['-webkit-transition'] = "-webkit-filter .50s"
-      document.body.style['-webkit-filter'] = "blur(150px) hue-rotate(20deg) saturate(14)"
+      document.body.style['-webkit-filter'] = "blur(150px) hue-rotate(20deg) saturate(11)"
 
       var log, rules, position, overflow;
 
