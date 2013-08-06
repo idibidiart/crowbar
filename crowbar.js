09d1f4c77495f617d2be70e2f5c3ae0fc23df08c
Marc Fawzi <marc.fawzi@gmail.com>
2013-08-06 00:07:21 -0700

    INITIAL

--- crowbar.js
+++ crowbar.js
@@ -98,7 +98,7 @@
             for (var j = 0; j < ss[i].cssRules.length; j++)
             {
                 if (ss[i].cssRules[j].type == window.CSSRule.FONT_FACE_RULE)
-                    retsult.push(s[i].cssRules[j].cssText)
+                    result.push(s[i].cssRules[j].cssText)
             }
     }
     return result;
