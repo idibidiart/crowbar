2a63588a356f524bf250763cf9ec177a4e69a586
Marc Fawzi <marc.fawzi@gmail.com>
2013-08-13 16:27:10 -0700

    INITIAL

--- crowbar.js
+++ crowbar.js
@@ -404,7 +404,7 @@
 
       console.log(log)
 
-      log = log.replace(new RegExp("([\\s]|^)([.]{1}[a-zA-Z0-9\\-_]+)(\\s*[>]*)", "gm"), function(m, p1, p2, p3, offset, string) {
+      log = log.replace(new RegExp("([\\s]|^)([.]{1}[a-zA-Z0-9\\-_]+)(\\s*[>]{1})", "gm"), function(m, p1, p2, p3, offset, string) {
 
           console.log('pre match', m)
           console.log('pre p2', p2)
@@ -413,7 +413,7 @@
               console.log('match', m)
               console.log('p2', p2)
               if (p3) {
-                  return " * > "
+                  return " ." + enclosingClass + " > "
               }
               return " "
           } else {
