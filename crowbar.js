57f8d1d478703c4398107cc3cf86b91f82e2669e
Marc Fawzi <marc.fawzi@gmail.com>
2013-08-13 12:15:49 -0700

    INITIAL

--- crowbar.js
+++ crowbar.js
@@ -404,7 +404,7 @@
 
       console.log(log)
 
-      log = log.replace(/[.]([a-z0-9\-_]+)(\s*[,]*)/g, function(m, p1, p2, offset, string) {
+      log = log.replace(/[^\d][.]([a-z0-9\-_]+)(\s*[,]*)/g, function(m, p1, p2, offset, string) {
 
           console.log('pre match', m)
 
